cbc4167f331b35389026015afe6617fb56c06dcb
Amanda <amanda.k.odonnell@gmail.com>
2019-04-28 17:43:02 -0400

    added index and information page
    still have to write up a good survey on the topic and clean some stuff up.  renamed old index.php page to survey.php

---
+++ disney_info.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Walt Disney World Information | Park Information</title>
+</head>
+
+<body>
+
+<header>
+    <h1><a href="index.php" id="logo"><img src="images/logo.jpg" alt="Disney World Logo"></a></h1>
+    <a href="survey.php" id="survey">Take Our Disney Survey!</a>
+</header>
+
+<main>
+    <section class="mk">
+        <h2>Magic Kingdom</h2>
+        <figure>
+            <img src="images/magic_kingdom.jpg" alt="Cinderella Castle">
+            <figcaption>Cinderella Castle: Magic Kingdom's Icon</figcaption>
+        </figure>
+        <h3 class="intro">Introduction</h3>
+        <p>Magic Kingdom is a theme park at the Walt Disney World Resort in Bay Lake, Florida, near Orlando. Owned and operated by The Walt Disney Company through its Parks, Experiences and Products division, the park opened on October 1, 1971, as the first of four theme parks at the resort. The park was initialized by Walt Disney and designed by WED Enterprises. Its layout and attractions are based on Disneyland Park in Anaheim, California, and are dedicated to fairy tales and Disney characters.</p>
+        <h3>Must See Attractions:</h3>
+        <ul>
+            <li>Space Mountain</li>
+            <li>Splash Mountain</li>
+            <li>Thunder Mountain</li>
+            <li>Buzz Lightyear's Space Ranger Spin</li>
+            <li>it's a small world</li>
+            <li>Haunted Mansion</li>
+            <li>Jungle Cruise</li>
+            <li>Pirates of the Caribbean</li>
+        </ul>
+    </section>
+
+    <section class="ep">
+        <h2>Epcot</h2>
+        <figure>
+            <img src="images/epcot.jpg" alt="Spaceship Earth">
+            <figcaption>Spaceship Earth: Epcot's Icon</figcaption>
+        </figure>
+        <h3 class="intro">Introduction</h3>
+        <p>Epcot is a theme park at the Walt Disney World Resort in Bay Lake, Florida. It is owned and operated by The Walt Disney Company through its Parks, Experiences and Products division. Inspired by an unrealized concept developed by Walt Disney, the park opened on October 1, 1982, as EPCOT Center, and was the second of four theme parks built at Walt Disney World, after the Magic Kingdom. Spanning 305 acres (123 ha), more than twice the size of the Magic Kingdom park, Epcot is dedicated to the celebration of human achievement, namely technological innovation and international culture, and is often referred to as a "permanent world's fair".</p>
+        <h3>Must See Attractions:</h3>
+        <ul>
+            <li>Soarin'</li>
+            <li>Test Track</li>
+            <li>Spaceship Earth</li>
+            <li>Mission: Space</li>
+            <li>Living with the Land</li>
+            <li>Flower and Garden Festival</li>
+            <li>Touring the World Showcase</li>
+            <li>The World Showcase Adventure</li>
+        </ul>
+    </section>
+
+    <section class="hs">
+        <h2>Hollywood Studios</h2>
+        <figure>
+            <img src="images/hollywood_studios.jpg" alt="The Tower of Terror">
+            <figcaption>The Tower of Terror: Hollywood Studios' Icon</figcaption>
+        </figure>
+        <h3 class="intro">Introduction</h3>
+        <p>Disney's Hollywood Studios is a theme park at the Walt Disney World Resort in Bay Lake, Florida, near Orlando. It is owned and operated by The Walt Disney Company through its Parks, Experiences and Products division. Based on a concept by Marty Sklar, Randy Bright, and Michael Eisner, the park opened on May 1, 1989, as the Disney-MGM Studios Theme Park, and was the third of four theme parks built at Walt Disney World. Spanning 135 acres (55 ha), the park is dedicated to the imagined worlds from film, television, music, and theatre, drawing inspiration from the Golden Age of Hollywood.</p>
+        <h3>Must See Attractions:</h3>
+        <ul>
+            <li>Star Tours</li>
+            <li>Muppet*Vision 3D</li>
+            <li>Toy Story Mania!</li>
+            <li>Slinky Dog Dash</li>
+            <li>Tower of Terror</li>
+            <li>Rock 'n' Roller Coaster</li>
+            <li>Beauty and the Beat Live on Stage</li>
+            <li>Indiana Jones Epic Stunt Spectactular!</li>
+        </ul>
+    </section>
+
+    <section class="ak">
+        <h2>Animal Kingdom</h2>
+        <figure>
+            <img src="images/animal_kingdom.jpg" alt="The Tree of Life">
+            <figcaption>The Tree of Life: Animal Kingdom's Icon</figcaption>
+        </figure>
+        <h3 class="intro">Introduction</h3>
+        <p>Disney's Animal Kingdom is a zoological theme park at the Walt Disney World Resort in Bay Lake, Florida, near Orlando. Owned and operated by The Walt Disney Company through its Parks, Experiences and Consumer Products division, it is the largest theme park in the world, covering 580 acres (230 ha). The park opened on Earth Day, April 22, 1998, and was the fourth theme park built at the resort. The park is dedicated and themed entirely around the natural environment and animal conservation, a philosophy once pioneered by Walt Disney himself.</p>
+        <h3>Must See Attractions:</h3>
+        <ul>
+            <li>Flight of Passage</li>
+            <li>Festival of the Lion King</li>
+            <li>Kilimanjaro Safaris</li>
+            <li>Expedition Everest</li>
+            <li>Kali River Rapids</li>
+            <li>Primeval Whirl</li>
+            <li>Dinosaur</li>
+            <li>Na'vi River Journey</li>
+        </ul>
+    </section>
+
+</main>
+
+    <footer>
+        <h2>Citations:</h2>
+        <ul>
+            <li><a href="http://csc174.org/midterm/aodonne3/">Images and Info from Amanda's Midterm</a></li>
+        </ul>
+        Team Manhattan | Assignment 7 | CSC 174
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,27 +1,34 @@
-<?php session_start(); 
-  $logged = true;
-  if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){ $logged = false; }
-?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Example using surveyjs</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://unpkg.com/jquery"></script>
-        <script src="https://surveyjs.azureedge.net/1.0.81/survey.jquery.js"></script>
-        <link href="https://surveyjs.azureedge.net/1.0.81/survey.css" type="text/css" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./index.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/styles.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Walt Disney World Information | Home</title>
     </head>
-    <body>
-        <?php include('nav.inc') ?>
-        <div style="margin-top: 50px; padding-bottom: 20px; border-radius: 10px; max-width: 800px;" class="container">
-        <h1 style="color: #67badb;"> Car Survey </h1>
-        <div id="surveyElement"></div>
-        <div id="surveyResult"></div>
-        <script type="text/javascript" src="./index.js"></script>
-        <div>
-    </body>
+
+<body>
+
+<header>
+    <h1><a href="index.php" id="logo"><img src="images/logo.jpg" alt="Disney World Logo"></a></h1>
+    <a href="disney_info.php" id="info">Learn More About Walt Disney World Parks!</a>
+</header>
+
+    <figure id="icons">
+        <img src="images/disney_world.jpg" alt="Disney World Icons">
+    </figure>
+
+    <div id="bottom"> <!--CHECK THAT THIS MAKES SEMANTIC SENSE-->
+        <div id="weak">"I only hope that we never lose sight of one thing — <br> that it was all started by a mouse." <br> — Walt Disney</div>
+
+        <a href="survey.php" id="survey">Take Our Disney Survey!</a>
+    </div>
+    
+    <footer>
+        <h2>Citations:</h2>
+        <ul>
+            <li><a href="http://csc174.org/midterm/aodonne3/">Images and Info from Amanda's Midterm</a></li>
+        </ul>
+        Team Manhattan | Assignment 7 | CSC 174
+    </footer>
+</body>
 </html>
